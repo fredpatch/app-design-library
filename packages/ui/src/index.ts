0a1d970b12a808c7fd47cfa2c9c413dff1a1b949
Carmel Fred PATCHELLI <40575184+fredpatch@users.noreply.github.com>
2026-07-24 16:20:10 +0200

    feat(ui): export FormField

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,5 +6,6 @@ export { Button } from './button/Button';
 export type { ButtonProps, ButtonSize, ButtonVariant } from './button/button.types';
 export { StatusBadge } from './status-badge/StatusBadge';
 export type { StatusBadgeProps } from './status-badge/status-badge.types';
+export { FormField } from './form-field/FormField';
 export type { FormFieldProps } from './form-field/form-field.types';
 export type { DataTableProps } from './data-table/data-table.types';
