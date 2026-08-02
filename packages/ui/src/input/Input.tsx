ec246a77f0448df13efb63d5ba063f36a207f2b6
Carmel Fred PATCHELLI <40575184+fredpatch@users.noreply.github.com>
2026-08-02 13:04:27 +0200

    feat(ui): add polished Input primitive

---
+++ packages/ui/src/input/Input.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { cn } from '../utils/cn';
+import './input.css';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  leadingIcon?: React.ReactNode;
+  trailingAction?: React.ReactNode;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, leadingIcon, trailingAction, disabled, ...props }, ref) => (
+    <span className="fp-input-shell" data-disabled={disabled ? 'true' : undefined}>
+      {leadingIcon ? <span className="fp-input-shell__icon" aria-hidden="true">{leadingIcon}</span> : null}
+      <input ref={ref} className={cn('fp-input', className)} disabled={disabled} {...props} />
+      {trailingAction ? <span className="fp-input-shell__action">{trailingAction}</span> : null}
+    </span>
+  ),
+);
+
+Input.displayName = 'Input';
