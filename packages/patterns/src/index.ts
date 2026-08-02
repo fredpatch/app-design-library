003265be5513d26c0ae7d885badbc57aa4f666ec
Carmel Fred PATCHELLI <40575184+fredpatch@users.noreply.github.com>
2026-08-02 13:27:35 +0200

    feat(entity-list-page): export page pattern

--- packages/patterns/src/index.ts
+++ packages/patterns/src/index.ts
@@ -4,4 +4,5 @@ export { FilterBar } from './filter-bar/FilterBar';
 export type { FilterBarProps } from './filter-bar/filter-bar.types';
 export { StatsGrid } from './stats-grid/StatsGrid';
 export type { StatItem, StatsGridProps } from './stats-grid/stats-grid.types';
+export { EntityListPage } from './entity-list-page/EntityListPage';
 export type { EntityListPageProps } from './entity-list-page/entity-list-page.types';
